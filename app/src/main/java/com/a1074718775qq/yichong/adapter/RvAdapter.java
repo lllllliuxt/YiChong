d6b9ce97cc72f78243f930e27bb7fc63aa8e5e17
liuxiaotong <1074718775@qq.com>
2018-01-29 21:00:05 +0800

    首页加入新闻cardview

---
+++ app/src/main/java/com/a1074718775qq/yichong/adapter/RvAdapter.java
@@ -0,0 +1,67 @@
+package com.a1074718775qq.yichong.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.a1074718775qq.yichong.R;
+import com.a1074718775qq.yichong.bean.PetNews;
+
+import java.util.List;
+
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.NewsViewHolder> {
+    Context mContext;
+    private List<PetNews> news;
+    public RvAdapter()
+    {
+
+    }
+    public RvAdapter(List<PetNews> news,Context mContext)
+    {
+        this.news=news;
+        this.mContext=mContext;
+    }
+    public static class NewsViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        ImageView image;
+        TextView title;
+        TextView introduction;
+
+        NewsViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.news_cardview);
+            image = (ImageView)itemView.findViewById(R.id.news_image);
+            title = (TextView)itemView.findViewById(R.id.news_title);
+            introduction = (TextView)itemView.findViewById(R.id.news_introduction);
+        }
+    }
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null){
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.cardview_item,parent,false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RvAdapter.NewsViewHolder holder, int position) {
+        holder.image.setImageResource(news.get(position).getImage());
+        holder.title.setText(news.get(position).getTitle());
+        holder.introduction.setText(news.get(position).getIntroduction());
+    }
+
+    @Override
+    public int getItemCount() {
+        return news.size();
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
--- app/src/main/java/com/a1074718775qq/yichong/fragment/HomeFragment.java
+++ app/src/main/java/com/a1074718775qq/yichong/fragment/HomeFragment.java
@@ -1,23 +1,27 @@
 package com.a1074718775qq.yichong.fragment;
 
 import android.content.Context;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 import com.a1074718775qq.yichong.R;
+import com.a1074718775qq.yichong.adapter.RvAdapter;
+import com.a1074718775qq.yichong.bean.PetNews;
 import com.a1074718775qq.yichong.widget.BannerViewHolder;
 import com.zhouwei.mzbanner.MZBannerView;
 import com.zhouwei.mzbanner.holder.MZHolderCreator;
-import com.zhouwei.mzbanner.holder.MZViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.v7.widget.LinearLayoutManager.*;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,7 +32,9 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
+    Context mContext=getActivity();
+    private List<PetNews> news;//对象列表
+    RecyclerView rv;
     View view;
     MZBannerView mMZBanner;
     //加入轮播图的图片，后期会用网络加入
@@ -82,6 +88,11 @@ public class HomeFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_home, container, false);
         findView();
         initBanner();
+        rv.setLayoutManager(new LinearLayoutManager(mContext, VERTICAL, false));
+
+        initializeData();
+        RvAdapter adapter = new RvAdapter(news,mContext);
+        rv.setAdapter(adapter);
         return view;
 
     }
@@ -99,8 +110,13 @@ public class HomeFragment extends Fragment {
             }
         });
     }
-
-
+//初始化新闻列
+    private void initializeData(){
+        news = new ArrayList<>();
+        news.add(new PetNews(R.drawable.photo1,"老太太与狗狗朝夕相伴多年，狗狗忽然跳楼，却不见老太太露面","她叫英，老伴去世的早，英为了供儿子念书，很长一段时间在城市的各大工地上做和水泥的工作。白天英顶着太阳在工地上拼命，....."));
+        news.add(new PetNews(R.drawable.photo2,"主人晒出猫咪剪发时的表情，微博网友瞬间笑喷：让这眼神委屈到哭！","最近微博网友爆料：家里养了一只长毛猫，毛主任想着最近冬天到了嘛，由于猫只要一换季节，猫毛就会掉的家里到处都是的，所......"));
+        news.add(new PetNews(R.drawable.photo3,"小偷入室偷窃，打伤金毛，事后为何还要主人赔偿？","据网友微博爆料成：事情发生在一个老小区，虽然小区没有门禁的，但是也还算和谐，基本上都没有出现过偷盗事件。由于临近年......"));
+    }
     public void onPause() {
         super.onPause();
         mMZBanner.pause();//暂停轮播
@@ -113,6 +129,7 @@ public class HomeFragment extends Fragment {
 
     private void findView() {
         mMZBanner = (MZBannerView) view.findViewById(R.id.home_banner);
+        rv= (RecyclerView)view.findViewById(R.id.news_list);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
